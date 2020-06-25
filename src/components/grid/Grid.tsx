216f99bda546801873c40ecad81b9cfe53e24b81
James Dickie <james.dickie@gmail.com>
2020-06-25 15:03:25 +0000

    add a utility grid

---
+++ src/components/grid/Grid.tsx
@@ -0,0 +1,27 @@
+import { HTMLElementList } from 'types/html-types';
+import c from 'classnames';
+import s from './grid.module.scss';
+
+interface IProps {
+  as?: HTMLElementList;
+  columnCount?: {mobile?: number, tablet?: number; desktop?: number};
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Grid = ({children, columnCount = {mobile: 1, tablet: 2, desktop: 3 }, as, className }: IProps) => {
+  const Wrapper = as ?? 'div';
+  const { mobile, tablet, desktop } = columnCount;
+
+  return (
+    <Wrapper className={c(
+      s.grid,
+      className,
+      s[`gridMobile${mobile}`],
+      s[`gridTablet${tablet}`],
+      s[`gridDesktop${desktop}`]
+    )}>
+      {children}
+    </Wrapper>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Date from 'components/date/Date';
 import { Container } from 'components/container/Container';
 import { H1, H2, H3 } from 'components/heading/Heading';
 import { Section } from 'components/section/Section';
+import { Grid } from 'components/grid/Grid';
 
 export default function Home({
   allPostsData
@@ -32,24 +33,21 @@ export default function Home({
       
         <Container>
           <H2>Blog</H2>
-          <ul>
+          <Grid as="ul">
             {allPostsData.map(({ id, date, title }) => (
-              <li key={id}>
+              <li key={id} style={{ background: '#fac', padding: 20}}>
                 <H3>
                   <Link href="/posts/[id]" as={`/posts/${id}`}>
                     <a>{title}</a>
                   </Link>
                 </H3>
                 
-                <br />
-                {id}
-                <br />
                 <Date dateString={date} />
               </li>
             )
             )}
             
-          </ul>
+          </Grid>
         </Container>
       </Section>
     </>
