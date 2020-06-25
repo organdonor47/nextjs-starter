fd6f78a661cfb749cbf0986ab64419ac73643ba5
James Dickie <james.dickie@gmail.com>
2020-06-25 11:31:50 +0000

    add local serif fonts

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -2,21 +2,23 @@ import { HTMLElementList } from 'types/html-types';
 
 import { Container } from 'components/container/Container';
 
+import c from 'classnames';
 import s from './section.module.scss';
 
 interface IProps {
   children: React.ReactNode;
   container?: boolean;
   as?: HTMLElementList;
+  className?: string;
 }
 
-export const Section = ({children, container = false, as = 'section'}: IProps) => {
+export const Section = ({children, container = false, as = 'section', className}: IProps) => {
   
   const SectionEl = as;
   const content = container ? <Container>{children}</Container> : children;
 
   return (
-    <SectionEl className={s.section}>
+    <SectionEl className={c(s.section, className)}>
       {content}
     </SectionEl>
   );
