a11469e64cbfc517b0b2a98b64fc778c9f263f23
James Dickie <james.dickie@gmail.com>
2020-07-06 14:50:57 +0000

    add video import

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const nextComposePlugins = require('next-compose-plugins');
 const nextReactSvg = require('next-react-svg');
 const nextImages = require('next-images');
+const nextVideos = require('next-videos');
 
 const svgDir = path.resolve(__dirname, 'src/assets/svg');
 
@@ -11,6 +12,7 @@ module.exports = nextComposePlugins(
   [
     [nextImages, { exclude: svgDir }],
     [nextReactSvg, { include: svgDir }],
+    nextVideos,
   ],
   nextConfig,
 );
