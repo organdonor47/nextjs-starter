76fa271ac46814d0c18315d04c13abf7541eab41
James Dickie <james.dickie@gmail.com>
2020-07-14 10:16:44 +0000

    fix mobile nav overflow; link should always close nav; add 404; put examples on index;

--- src/containers/nav/Nav.tsx
+++ src/containers/nav/Nav.tsx
@@ -6,7 +6,6 @@ import { NavLink } from 'components/nav/NavLink';
 
 export const NavContainer = () => (
   <Nav>
-    <NavLink to="/examples">Examples</NavLink>
     <NavLink to="/posts/[id]" as={`/posts/markdown-kitchen-sink`}>HTML elements</NavLink>
     <NavLink to="/forms">Form elements</NavLink>
   </Nav>
---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { H1 } from 'components/heading/Heading';
+import { Section } from 'components/section/Section';
+
+export default function Custom404() {
+  return (
+  <Section container>
+    <H1>404 - Page Not Found</H1>
+  </Section>
+  )
+}
